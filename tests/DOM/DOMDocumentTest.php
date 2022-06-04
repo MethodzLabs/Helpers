3a5c1d27d8f39b6abfbabead4f5099ae760b9681
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2022-06-04 17:16:15 +0200

    add DOMDocument

---
+++ tests/DOM/DOMDocumentTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DOM;
+
+use Methodz\Helpers\DOM\DOMDocument;
+use PHPUnit\Framework\TestCase;
+
+class DOMDocumentTest extends TestCase
+{
+
+	public function testFromHTMLFile()
+	{
+		$domDocument = DOMDocument::fromHTMLFile(__DIR__."/../data", "test.html");
+		print_r($domDocument->findElementsByXPath("//div")[0]->parentNode);
+		self::assertTrue(true);
+	}
+
+	public function testFindElementsByXPath()
+	{
+
+	}
+}
