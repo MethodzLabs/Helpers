3527d8bd012498aa50ebd27ab3ab7c27710994a1
ThomasFT3721 <thomas.fontaine.tuffery@gmail.com>
2022-06-28 15:54:09 +0200

    Update Status.php

--- src/Methodz/Helpers/Type/Status.php
+++ src/Methodz/Helpers/Type/Status.php
@@ -7,4 +7,19 @@ enum Status
 	case OK;
 	case PENDING;
 	case ERROR;
+
+	public function isOk(): bool
+	{
+		return $this === self::OK;
+	}
+
+	public function isPending(): bool
+	{
+		return $this === self::PENDING;
+	}
+
+	public function isError(): bool
+	{
+		return $this === self::ERROR;
+	}
 }
